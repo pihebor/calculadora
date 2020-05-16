34ee8401abd205778f5525459ec67f22cf3f3f7c
Pedro Hernandez <pihebor@hotmail.com>
2020-05-16 17:26:28 -0500

    Aplicacion sumando corrctamente

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText numero_uno, numero_dos;
+    private TextView resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numero_uno=findViewById(R.id.txtNumeroUno);
+        numero_dos=findViewById(R.id.txtNumeroDos);
+        resultado= findViewById(R.id.lblResultado);
     }
+
+    public void calcular(View v){
+        int num_uno, num_dos, suma;
+        num_uno= Integer.parseInt(numero_uno.getText().toString());
+        num_dos= Integer.parseInt(numero_dos.getText().toString());
+        suma= num_uno + num_dos;
+        resultado.setText(""+suma);
+    }
+
 }
